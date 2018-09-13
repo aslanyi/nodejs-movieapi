517bfe121fdb52ea63981487d1e34b1fb98d7720
Taha TEPEDELEN <mustafatahatepedelen@hotmail.com>
2018-09-13 14:35:16 +0300

    bcryptjs

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express= require('express'),
       router = express.Router();
 
-      const bcrypt = require('bcrypt');
+      const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 const jwt = require('jsonwebtoken');
